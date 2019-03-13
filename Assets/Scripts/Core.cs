4752ec43113bd05acaedafdd97dba02a0353d7b2
ttotti <prs12344@naver.com>
2019-03-13 23:19:22 +0900

    2019/03/13

---
+++ Assets/Scripts/Core.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Core : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // 오브젝트의 자식 콜라이더가 다른 콜라이더에 충돌했을때 호출합니다
+    // 관련된 해당 오브젝트에 대한 추가 정보는 호출동안에 전달된 Collision2D 파리미터를 통해 보고됩니다. 이 정보가 필요없다면, 해당 파라미터없이 OnCollisionEnter2D를 선언할 수 있습니다
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Tank_Shell")
+        {
+            Debug.Log(">>>>>>>>>>>> Hit!");
+            transform.localScale += new Vector3(1.0f, 1.0f, 1.0f);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(1000.0f,-1000.0f));
+        }
+    }
+}
---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tank : MonoBehaviour {
+
+    GameObject goShell = null;
+    bool action = false;
+
+	// Use this for initialization
+	void Start () {
+        // 포탄 게임 오브젝트를 가져오고
+        goShell = transform.Find("Tank_Shell").gameObject;
+        // 포탄을 표시하지 않게 설정
+        goShell.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 버튼을 눌렀는가?
+		if(Input.GetMouseButton(0))
+        {
+            // 탱크를 클릭했는가?
+            // 카메라를 기준으로 마우스 포인트를 얻어오고
+            Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // 해당 마우스 포인트에 콜라이더가 있는지 확인한다
+            Collider2D collition2d = Physics2D.OverlapPoint(tapPoint);
+
+            // 있을 경우
+            if(collition2d)
+            {
+                if (collition2d.gameObject == gameObject)
+                {
+                    // 액션 유효화
+                    action = true;
+                }
+            }
+
+            // 버튼을 누른 상태인가?
+            if(action)
+            {
+                // 탱크 이동
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(+30.0f, 0.0f));
+            }
+        }
+        else
+        {
+            // 버튼을 놓았는가?
+            if (Input.GetMouseButtonUp(0) && action)
+            {
+                // 포탄 발사
+                if(goShell)
+                {
+                    // 포탄 표시
+                    goShell.SetActive(true);
+                    goShell.GetComponent<Rigidbody2D>().AddForce(new Vector2(+300.0f, 500.0f));
+
+                    // 3초 후 포탄오브젝트를 지운다
+                    Destroy(goShell.gameObject, 3.0f);
+                }
+
+                action = false;
+            }
+        }
+
+	}
+
+    public void OnGUI()
+    {
+        GUI.TextField(new Rect(10, 10, 300, 60), "[Unity 2D Sample 2-1]\n" + "탱크를 클릭하면 가속\n놓으면 발사!");
+
+        if (GUI.Button(new Rect(10, 80, 100, 20), "다시 시작"))
+        {
+            // 씬 전환함수 Application.LoadLevel() 함수는 이제 사용하지 않는다
+            // Application.LoadLevel(Application.loadedLevelName);
+
+            // 대신 다음 함수를 사용한다
+            // using using UnityEngine.SceneManagement; 을 포함해주고
+            // SceneManger.LoadScene("씬 이름");
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
